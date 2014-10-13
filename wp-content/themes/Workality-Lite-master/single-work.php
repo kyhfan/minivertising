a7b5f2da9ee1a4057f6872bf9d92cd02157da558
kyhfan <yh.kim@minivertising.kr>
2014-10-13 17:38:07 +0900

    WORK카테고리 동영상 iframe 크기 조절
    동영상 iframe 삽입시 id="video_ifrm" 만 추가해 주시면
    모바일에서 자동적으로 높이가 300px로 변경이 됩니다.

--- wp-content/themes/Workality-Lite-master/single-work.php
+++ wp-content/themes/Workality-Lite-master/single-work.php
@@ -67,6 +67,15 @@
   </div>
 </div> 
 
+<script type="text/javascript">
+$(window).resize(function(){
+  var b_width = document.body.clientWidth;
+
+	if (b_width < 960){
+		$("#video_ifrm").css("height","300px"); 
+	}
+}).resize();
+</script>
 
 <?php
 	include_once("_footer.php");
