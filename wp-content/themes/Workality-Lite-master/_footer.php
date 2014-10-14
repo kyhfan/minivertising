dff5261649a964118b6de6e5c07270b6c3372423
minivertising <sh.yang@minivertising.kr>
2014-10-14 15:51:54 +0900

    footer 위치 변경

--- wp-content/themes/Workality-Lite-master/_footer.php
+++ wp-content/themes/Workality-Lite-master/_footer.php
@@ -5,10 +5,11 @@
         <div class="clearfix">
               <div class="text_1">
                 <p>Agency for MINI BRAND, MINIVERTISING.</p>
-				<div class="fb-like" data-href="http://www.facebook.com/minivertising" data-layout="button_count" data-action="like" data-show-faces="true" data-share="false" style="overflow:hidden;"></div>
+				© MINIVERTISING Corp.
               </div>
               <div class="text_copy">
-                © MINIVERTISING Corp.
+				<div class="fb-like" data-href="http://www.facebook.com/minivertising" data-layout="button_count" data-action="like" data-show-faces="true" data-share="false" style="overflow:hidden;"></div>
+                
               </div>
               
         </div>
