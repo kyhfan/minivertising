85680d84f40a28ef6bcb6617284ec6ec1469ac42
kyhfan <yh.kim@minivertising.kr>
2014-10-14 13:22:29 +0900

    푸터 페이스북 라이크버튼 추가

--- wp-content/themes/Workality-Lite-master/_footer.php
+++ wp-content/themes/Workality-Lite-master/_footer.php
@@ -5,6 +5,7 @@
         <div class="clearfix">
               <div class="text_1">
                 <p>Agency for MINI BRAND, MINIVERTISING.</p>
+				<div class="fb-like" data-href="http://minivertising.kr" data-layout="button_count" data-action="like" data-show-faces="true" data-share="false"></div>
               </div>
               <div class="text_copy">
                 © MINIVERTISING Corp.
