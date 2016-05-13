7e57f9325c0190f57f0e8d5941f84169691f6246
kyh <kyhfan@gmail.com>
2016-05-13 12:45:28 +0900

    메인 변경

--- wp-content/themes/Workality-Lite-master/page-main.php
+++ wp-content/themes/Workality-Lite-master/page-main.php
@@ -1,19 +1,81 @@
 <?php
 	include_once("_header.php");
-	echo "<script>location.href='".$home."/work/';</script>"
+	
+	// thumbnail 크기 설정
+	//add_theme_support( 'post-thumbnails' );
+	//set_post_thumbnail_size( 220, 160, true );
+	add_image_size('custom-size', 220, 160, true );
 ?>
-    <div id="post-list" style="margin-top:0px">
-      <br class="clear" />
-      <div class="fitvids">
-        <div class="sixteen columns main_page">
-        	<div class="inner">
-        	MINI IS BIG.<br>
-        	MINI IS ORIGINAL.<br>
-        	MINI IS FUN.
-            </div>
-         </div>   
+    <div id="post-list" class="row" style="margin-top:0px">
+
+<?php
+	//query_posts('cat=8');
+	query_posts('cat=4');
+	while (have_posts()) : the_post();
+		$client_array = get_post_meta( $post->ID, "client");
+?>
+      <div id="div_post" class="four columns featured project-item post_test clearfix" >
+        <div class="imgdiv">
+          <a href="<?= the_guid()?>" class="getworks" data-type="works" data-id="22" data-token="5b4ac08af1">
+            <span></span>
+<?php
+	if (has_post_thumbnail())
+	{
+		the_post_thumbnail('custom-size');
+	}else{
+?>
+            <img src="<?=$home?>/wp-content/themes/Workality-Lite-master/images/no-image.jpg" data-small="<?=$home?>wp-content/themes/Workality-Lite-master/images/no-image.png-150x113.jpg" data-large="<?=$home?>/wp-content/themes/Workality-Lite-master/images/no-image.png-300x226.jpg" title="<?=the_title()?>" alt="<?=the_title()?>" />
+<?php
+	}
+?>
+          </a>
+        </div>
+        <div class="thumb_large">
+          <h5><a href="<?= the_guid()?>" class="getworks" data-type="works" data-id="22" data-token="5b4ac08af1"><?=the_title()?></a></h5>
+          <p><?=$client_array[0]?></p>
+          <!-- <p><?=the_excerpt()?></p> -->
+        </div>  
       </div>
-    </div>
+<?php
+	endwhile;
+?>
+      <br class="clear rowseperator">
+    </div>  
+<script type="text/javascript">
+/*
+$(window).resize(function(){
+	var b_width = $("#div_post").width();
+
+	if (b_width == 220)
+		<? set_post_thumbnail_size( 220, 160, true );?>;
+	else
+		<? set_post_thumbnail_size( 172, 130, true );?>;
+
+}).resize();
+*/
+var body_width = 0;
+$(document).ready(function(){
+	body_width	= $("body").width();
+	if (body_width > 960 )
+	{
+		$(".post_test ").css("height","260px");
+	}else{
+		$(".post_test ").css("height","");
+	}
+});
+
+$(window).resize(function(){
+	body_width	= $("body").width();
+	if (body_width > 960 )
+	{
+		$(".post_test ").css("height","260px");
+	}else{
+		$(".post_test ").css("height","");
+	}
+}).resize();
+
+</script>
+
 <?php
 	include_once("_footer.php");
 ?>
---
+++ wp-content/themes/Workality-Lite-master/page-main_bak_20160513.php
@@ -0,0 +1,19 @@
+<?php
+	include_once("_header.php");
+	echo "<script>location.href='".$home."/work/';</script>"
+?>
+    <div id="post-list" style="margin-top:0px">
+      <br class="clear" />
+      <div class="fitvids">
+        <div class="sixteen columns main_page">
+        	<div class="inner">
+        	MINI IS BIG.<br>
+        	MINI IS ORIGINAL.<br>
+        	MINI IS FUN.
+            </div>
+         </div>   
+      </div>
+    </div>
+<?php
+	include_once("_footer.php");
+?>
